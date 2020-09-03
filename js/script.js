c2f872d62fef3403c10ba91fbc5704b9b80896ce
sean.hay <sean.hay@gmail.com>
2020-09-03 21:41:16 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const endDate = new Date("2021-08-17")
+let counterBox = document.getElementById("counter")
+
+setInterval(() => {
+	let nowDate = new Date()
+	let timeDiff = countdown(nowDate, endDate)
+	// counterBox.textContent = `${timeDiff.years}/${timeDiff.months}/${timeDiff.days}/${timeDiff.hours}/${timeDiff.minutes}/${timeDiff.seconds}/${timeDiff.ms}`
+	// counterBox.innerHTML = ""
+	for (const [key, value] of Object.entries(timeDiff)) {
+		let val = '' ? '0' : value
+		let container = document.querySelector(`.${key}`)
+		if (container.textContent != val)
+			container.textContent = val
+		//counterBox.innerHTML += `<span class="${key}" data-key="${key}">${value}</span>`
+	}
+}, 10)
+
+// let nowDate = new Date()
+// let timeDiff = countdown(nowDate, endDate)
+// console.log(timeDiff)
+
+
+function countdown(fromDate, targetDate) {
+	let fromTime = fromDate.getTime()
+	let toTime = targetDate.getTime()
+	let duration = toTime - fromTime
+	let years = Math.floor(duration / 3.154e+10)
+	let durationMinusYears = duration - (years * 3.154e+10)
+	let months = Math.floor(duration / 2.628e+9) % 12
+	let durationMinusMonths = durationMinusYears - (months * 2.628e+9)
+	let days = Math.floor(durationMinusMonths / 8.64e+7)
+	let hours = Math.floor(duration / 3.6e+6) % 24
+	let mins = Math.floor(duration / 60000) % 60
+	let secs = Math.floor(duration / 1000) % 60
+	let ms = String(Math.floor(duration) % 1000).padStart(2, '0')
+
+	return { "years": years, "months": months, "days": days, "hours": hours, "minutes": mins, "seconds": secs, "ms": ms }
+}
